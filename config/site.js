0ee9bac64e573a0330b9cdca6802d85b5d445aa1
Thomas Lekanger <datagutt@lekanger.no>
2019-10-08 11:18:13 +0200

    Config: Fix API URL

--- config/site.js
+++ config/site.js
@@ -1,5 +1,5 @@
 export default {
-	baseURL: process.env.NODE_ENV === 'production' ? 'https://api.letswatch.video' : 'http://127.0.0.1:3000',
+	baseURL: process.env.NODE_ENV === 'production' ? 'https://api.letswatch.video' : 'http://127.0.0.1:9000',
 	name: 'Let\'s Watch',
 	dsn: process.env.SENTRY_DSN,
 	debug: true
